c37c80f0322186473ffb80b0dccd823811318688
Skylar Pape <11418651+skylarpape@users.noreply.github.com>
2023-11-05 20:50:18 -0500

    Adds format command that has access to config and recipes dir path

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# scripts
-Scripts for personal workflows
+# Personal Scripts
+This project contains any scripts I use in my personal workflows.
+Check out this README to understand the development environment it uses.
+Checkout the docs in the list below for details on each individual script:
+
+- [Mela](./mela/README.md)
+
+## Python Environment
+
---
+++ mela/yaml_config.py
@@ -0,0 +1,49 @@
+import os
+import yaml
+from pathlib import Path
+
+CONFIG_PATH = f"{Path(os.path.dirname(os.path.realpath(__file__)))}/config.yaml"
+
+ALL_VALIDATORS = {
+    'format': {
+        'ingredients': {
+            'prefixes': (
+                lambda value: type(value) is dict and all(type(item) is str for item in value.values()),
+                "must be map<string, string>"
+            )
+        }
+    }
+}
+
+
+class YamlConfigMissingKeyException(Exception):
+    """Exception raised when YAML config is missing a required key"""
+    def __init__(self, yaml_keys_string):
+        super().__init__(f"Key {yaml_keys_string} missing from {CONFIG_PATH}")
+
+
+class YamlConfigInvalidTypeException(Exception):
+    """Exception raised when contents of YAML config have an invalid type"""
+    def __init__(self, yaml_keys_string, requirement_message):
+        super().__init__(f"{yaml_keys_string} {requirement_message}")
+
+
+def validate(validators, content, parent_yaml_keys_string=""):
+    for key, value in validators.items():
+        yaml_keys_string = f"{parent_yaml_keys_string}.{key}" if parent_yaml_keys_string else key
+        if type(value) is tuple:
+            is_valid, requirement_message = value
+            if not is_valid(content[key]):
+                raise YamlConfigInvalidTypeException(yaml_keys_string, requirement_message)
+            return True
+        else:
+            if key not in content.keys():
+                raise YamlConfigMissingKeyException(yaml_keys_string)
+            return validate(validators[key], content[key], yaml_keys_string)
+
+
+def get_config():
+    with open(CONFIG_PATH, "r") as yaml_file:
+        content = yaml.safe_load(yaml_file)
+    validate(ALL_VALIDATORS, content)
+    return content
